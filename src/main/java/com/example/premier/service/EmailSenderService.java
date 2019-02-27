d0430f94ccee18208348b1b5fe84e835b80b5a69
Sanya Babatunde <jeromebabatunde@gmail.com>
2019-02-27 00:43:55 -0500

    create emailSenderService class and added relative proprties to application properties file

---
+++ src/main/java/com/example/premier/service/EmailSenderService.java
@@ -0,0 +1,27 @@
+package com.example.premier.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+import org.springframework.mail.javamail.JavaMailSender;
+
+
+@Service
+public class EmailSenderService {
+
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailSenderService(JavaMailSender javaMailSender){
+        this.javaMailSender = javaMailSender;
+
+    }
+
+    public void sendMail(SimpleMailMessage email){
+        javaMailSender.send(email);
+
+    }
+
+
+
+}
